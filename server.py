427a71feb6006412d276c0afc0cb7c96b7ef6b9f
deepakxyz <deepakrajan.grea@gmail.com>
2021-10-24 20:20:20 +0530

    first-push

---
+++ server.py
@@ -0,0 +1,22 @@
+from telegram import * 
+from telegram.ext import *
+
+TOKEN = '2057573270:AAH-33QCuMRQLehNRq6mbdS9R5xvUoAfba0'
+
+bot = Bot(TOKEN)
+updater = Updater(TOKEN, use_context=True)
+
+dispatcher = updater.dispatcher
+
+def test(update:Update, context:CallbackContext):
+	bot.send_message(
+		chat_id = update.effective_chat.id,
+		text="Hey Aishoo. Will update you with the orders details",
+	)
+
+
+start_value = CommandHandler('oi', test)
+
+dispatcher.add_handler(start_value)
+
+updater.start_polling()
